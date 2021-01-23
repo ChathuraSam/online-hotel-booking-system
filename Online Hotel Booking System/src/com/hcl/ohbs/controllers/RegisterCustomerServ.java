447e5c2089e342c615b2a642e1e3d1f600554ea2
shanukaDinushi <shanudi1994@gmail.com>
2021-01-23 20:47:47 +0530

    add register customer backend code 3

---
+++ Online Hotel Booking System/src/com/hcl/ohbs/controllers/RegisterCustomerServ.java
@@ -0,0 +1,34 @@
+package com.hcl.ohbs.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hcl.ohbs.services.RegisterCustomerService;
+
+@WebServlet("/RegisterCustomerServ")
+public class RegisterCustomerServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        RegisterCustomerService registerCustomer = new RegisterCustomerService();
+        try {
+			out.println(registerCustomer.registerCustomer());
+		} catch (ClassNotFoundException | SQLException e) {
+			
+			e.printStackTrace();
+		}
+        out.println("</body></html>");
+	}
+
+
+}
