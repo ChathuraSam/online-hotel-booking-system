08ef83841c639b3bef479bacb521d89ef391fe5d
shanukaDinushi <shanudi1994@gmail.com>
2021-01-24 15:46:36 +0530

    add hotel and sql changes

--- Online Hotel Booking System/src/com/hcl/ohbs/controllers/LogInHotelOwner.java
+++ Online Hotel Booking System/src/com/hcl/ohbs/controllers/LogInHotelOwner.java
@@ -1,35 +1,37 @@
 package com.hcl.ohbs.controllers;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.hcl.ohbs.services.HotelOwnerService;
-@WebServlet("/RegisterHotelOwner")
-public class RegisterHotelOwner extends HttpServlet {
-	
+@WebServlet("/LogInHotelOwner")
+public class LogInHotelOwner extends HttpServlet {
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("<html><boby>");
-		HotelOwnerService hotelOwnerService = new HotelOwnerService();
-		if(hotelOwnerService.registerHotelOwner("Shanuka", "Hettiarachchi", "946786934V", "0715678974", "abc@gmail.com", "username", "password")) {		
+		HotelOwnerService hotelOwnerService = new HotelOwnerService();		
+		if(hotelOwnerService.logInHotelOwner("username", "pass")) {
 			int id = hotelOwnerService.getIdByUsernameAndPassword("username", "password");
-			//if(id>0) {
-				out.println("<font>hotel owner registration success!!<font>");
-			//}else{
-				//out.println("<font color='red'>internal error! try again!<font>");
-			//}
-			//call ownerHome.jsp and pass the id in session
+			if(id>0) {
+				out.println("<font>hotel owner login success!!<font>");
+				//call ownerHome.jsp and pass the id in session
+			}else{
+				out.println("<font color='red'>internal error! try again!<font>");
+			}			
 		}else {
-			out.println("<font color='red'>Error in registering the hotel owner<font>");
+			out.println("<font color='red'>Error in login the hotel owner<font>");
 		}
 		out.println("</boby><html>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
 	}
 
 }
--- Online Hotel Booking System/src/com/hcl/ohbs/services/HotelOwnerService.java
+++ Online Hotel Booking System/src/com/hcl/ohbs/services/HotelOwnerService.java
@@ -3,8 +3,29 @@ import com.hcl.ohbs.dao.HotelOwnerDAO;
 import com.hcl.ohbs.entities.HotelOwner;
 public class HotelOwnerService {
 	
-	public boolean registerHotelOwner() {
+	public boolean registerHotelOwner(String fistName, String lastName, String nic, String phoneNumber, String email, String username, String password) {
 		HotelOwnerDAO h = new HotelOwnerDAO();
-		return h.addHotelOwner(new HotelOwner("Shanuka", "Hettiarachchi", "946786934V", "0715678974", "abc@gmail.com", "username", "password"));
+		return h.addHotelOwner(new HotelOwner(fistName, lastName, nic, phoneNumber, email, username, password));
+	}
+	
+	public boolean logInHotelOwner(String username, String password) {
+		HotelOwnerDAO h = new HotelOwnerDAO();
+		boolean isLogInSuccess = false;
+		if(h.logInHotelOwner(username)==null) {
+			System.out.println("Incorrect username");
+			isLogInSuccess = false;
+		}else if(!h.logInHotelOwner(username).getPassword().equals(password)) {
+			System.out.println("username and passwrod not matching");
+			isLogInSuccess = false;
+		}else{
+			System.out.println("log in success");
+			isLogInSuccess = true;
+		}
+		return isLogInSuccess;
+	}
+	
+	public int getIdByUsernameAndPassword(String username, String password) {
+		HotelOwnerDAO h = new HotelOwnerDAO();
+		return h.findIdByUsernamePassword(username, password);
 	}
 }
