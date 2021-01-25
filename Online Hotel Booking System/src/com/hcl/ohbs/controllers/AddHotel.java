51206d6483f2b8960cc1379adebf7f53608a21f5
Chathurasam <chathura.samarajeewa@gmail.com>
2021-01-26 00:53:34 +0530

    added jar files for file uploading

--- Online Hotel Booking System/src/com/hcl/ohbs/controllers/AddHotel.java
+++ Online Hotel Booking System/src/com/hcl/ohbs/controllers/AddHotel.java
@@ -33,7 +33,7 @@ public class AddHotel extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		doGet(request, response);
 	}
 
 }
